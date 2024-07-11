e2acbe31c167ae2fac289df5120446b31507dfe5
Gokulcasper <ronitcasper215@gmail.com>
2024-07-11 23:26:17 +0530

    color Picker task

---
+++ index.js
@@ -0,0 +1,88 @@
+let ContainerE1 = document.getElementById("colorPickerContainer");
+ContainerE1.classList.add("containerClass");
+
+let selectedColorHexCode = document.getElementById("selectedColorHexCode")
+selectedColorHexCode.style.color="#49a6e9"
+
+let btn1 = document.getElementById("button1");
+btn1.style.backgroundColor="#e0e0e0"
+
+
+let btn2 = document.getElementById("button2");
+btn2.style.backgroundColor="#6fcf97"
+
+let btn3 = document.getElementById("button3");
+btn3.style.backgroundColor="#56ccf2"
+
+let btn4 = document.getElementById("button4");
+btn4.style.backgroundColor="#bb6bd9"
+
+btn1.onClick = function(){
+    // ContainerE1.style.backgroundColor="#e0e0e0";
+    selectedColorHexCode.style.color="#e0e0e0";
+}
+console.log(ContainerE1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let ContainerE1 = document.getElementById("containerId");
+// ContainerE1.classList.add("containerClass");
+
+// let ContainerE2 = document.getElementById("container2Btn");
+// ContainerE1.classList.add("containerClassBtn");
+
+// let h1Container = document.createElement("h1");
+// h1Container.textContent="Color Picker";
+// ContainerE1.appendChild(h1Container);
+
+
+//Create Button And Its Class
+// let btn1Container2=document.createElement("button");
+// btn1Container2.classList.add("containerClassBtn");
+// let btn2Container2=document.createElement("button");
+// btn1Container2.classList.add("containerClassBtn");
+// let btn3Container2=document.createElement("button");
+// btn1Container2.classList.add("containerClassBtn");
+// let btn4Container2=document.createElement("button");
+// btn1Container2.classList.add("containerClassBtn");
+
+//Assign Button Values
+// btn1Container2.textContent="#e0e0e0";
+// btn2Container2.textContent="#6fcf97";
+// btn3Container2.textContent="#56ccf2";
+// btn4Container2.textContent="#bb6bd9";
+
+//Button Append Class
+// ContainerE1.appendChild(ContainerE2);
+// ContainerE2.appendChild(btn1Container2);
+// ContainerE2.appendChild(btn2Container2);
+// ContainerE2.appendChild(btn3Container2);
+// ContainerE2.appendChild(btn4Container2);
+
+
+
+
